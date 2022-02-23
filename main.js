3a71698858e53dc3f1c5c52e3b0b5548901089c4
sohilp2010 <77651250+sohilp2010@users.noreply.github.com>
2022-02-23 16:55:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music_1 = "";
 music_2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 preload()
 {
@@ -14,9 +18,22 @@ setup()
 
     video = createCanvas(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-draw()
+function gotPoses(results)
 {
-    
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY" + rightWristY);
+    }
 }
